73a566712680449ceeaca037ef242196a7a56d8c
Artem <baza.a1@bk.ru>
2024-06-24 16:11:28 +0300

    Доработал(fix)

--- service/utils.py
+++ service/utils.py
@@ -11,31 +11,43 @@ import smtplib
 def send_mailing():
     zone = pytz.timezone(settings.TIME_ZONE)
     current_datetime = datetime.now(zone)
-    mailings = Mailing.objects.filter(time_sending=current_datetime, status=Mailing.status_chose)
+
+    mailings = Mailing.objects.filter(time_sending=current_datetime).filter(
+        status=['created', 'launched'])
 
     for mailing in mailings:
-        last_attempt = Attempt.objects.filter(mailing=mailing).order_by('-attempt_date').first()
-        next_send_time = current_datetime
+        if mailing.time_sending is None:
+            mailing.time_sending = current_datetime
+        title = mailing.message.subject
+        content = mailing.message.text
+        mailing.status = 'launched'
+        mailing.save()
 
-        if mailing.time_sending <= current_datetime:
-            server_response = send_mail(
-                subject=title,
-                message=content,
-                from_email=settings.EMAIL_HOST_USER,
-                recipient_list=[recipient.email for recipient in mailing.recipients.all()],
-                fail_silently=False,
-            )
-            if server_response == 1:
-                server_response = 'Письмо успешно отправлено'
-            Attempt.objects.create(status='success', answer=server_response, mailing=mailing)
+        try:
+            if mailing.time_end < mailing.time_sending:
+                mailing.time_sending = current_datetime
+                mailing.status = 'completed'
+                mailing.save()
+                continue
 
-            if mailing.periodicity == Mailing.DAILY:
-                next_send_time = last_attempt.attempt_date + timedelta(days=1) if last_attempt else current_datetime
-            elif mailing.periodicity == Mailing.WEEKLY:
-                next_send_time = last_attempt.attempt_date + timedelta(weeks=1) if last_attempt else current_datetime
-            elif mailing.periodicity == Mailing.MONTHLY:
-                next_send_time = last_attempt.attempt_date + timedelta(weeks=4) if last_attempt else current_datetime
+            if mailing.time_sending <= current_datetime:
+                server_response = send_mail(
+                    subject=title,
+                    message=content,
+                    from_email=settings.EMAIL_HOST_USER,
+                    recipient_list=[recipient.email for recipient in mailing.clients.all()],
+                    fail_silently=False,
+                )
+                if server_response == 1:
+                    server_response = 'Письмо успешно отправлено'
+                Attempt.objects.create(status='successful', answer=server_response, mailing=mailing)
 
-            if current_datetime >= next_send_time:
-                send_email(mailing.message.subject, mailing.message.body, [clients.email for clients in mailing.clients.all()])
-                Attempt.objects.create(newsletter=mailing, attempt_date=current_datetime)
+                if mailing.periodicity == 'everyday':
+                    miling.time_sending = current_datetime + timedelta(days=1)
+                elif mailing.periodicity == 'once a week':
+                    miling.time_sending = current_datetime + timedelta(days=7)
+                elif mailing.periodicity == 'один раз в месяц':
+                    miling.time_sending = current_datetime + timedelta(months=1)
+
+        except smtplib.SMTPException as error:
+            Attempt.objects.create(status='unsuccessful', answer=error, mailing=mailing)
